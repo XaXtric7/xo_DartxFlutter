d3fea5bef74baa8696dc88c4ebb54ad198d56b6c
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-03-23 01:31:45 +0530

    maps_in_dart

---
+++ Dart_2/dart_app2/lib/maps.dart
@@ -0,0 +1,15 @@
+// Maps in Dart
+// Stores data in ordered way
+// Key : Value format of storage
+// key will be unique
+class Test {
+  add() {
+    Map<String, String> mapdata = {"Name": "Sarthak", "Surname": "Dharmik"};
+    print(mapdata);
+  }
+}
+
+void main() {
+  Test obj = Test();
+  obj.add();
+}
