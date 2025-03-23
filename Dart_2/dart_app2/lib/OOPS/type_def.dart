aa0b60206a792ba87b5d8c64b245b814db955fbc
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-03-23 17:19:32 +0530

    typedef_in_dart

---
+++ Dart_2/dart_app2/lib/OOPS/type_def.dart
@@ -0,0 +1,18 @@
+// Creating user defined functions
+// alias
+typedef Temp(int a);
+
+First(int a) {
+  print("First function is called ${a + 1}");
+}
+
+Second(int a) {
+  print("Second function is called ${a + 2}");
+}
+
+void main() {
+  Temp obj = First;
+  obj(2);
+  obj = Second;
+  obj(3);
+}
