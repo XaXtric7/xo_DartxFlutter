ca06223e5cd2c8a90932103f3ea7e7b9a28f20a1
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-03-23 17:23:45 +0530

    generics_dart

---
+++ Dart_2/dart_app2/lib/OOPS/generics.dart
@@ -0,0 +1,12 @@
+class Test {
+  list() {
+    List<dynamic> listItems = [1, 2, 3, 4];
+    listItems.add("Sarthak");
+    print(listItems);
+  }
+}
+
+void main() {
+  Test obj = Test();
+  obj.list();
+}
